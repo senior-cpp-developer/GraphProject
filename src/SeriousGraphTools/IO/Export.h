6d40be4aa613250a0ca697b30c64d2a562ed55a7
senior-cpp-developer <58088610+senior-cpp-developer@users.noreply.github.com>
2021-05-31 19:22:31 +0200

    Added skeleton for export

---
+++ src/SeriousGraphTools/IO/Export.h
@@ -0,0 +1,19 @@
+#ifndef GRAPHPROJECT_EXPORT_H
+#define GRAPHPROJECT_EXPORT_H
+
+#include "../../libs/nlohmann/json.hpp"
+#include "../SimpleGraph.h"
+
+// for convenience
+using json = nlohmann::json;
+
+namespace SeriousGraphTools {
+    class Export {
+        static json exportToIncidenceMatrix(const SimpleGraph &graph) {
+
+        }
+    };
+}
+
+
+#endif //GRAPHPROJECT_EXPORT_H
